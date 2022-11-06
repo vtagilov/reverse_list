cb4525426a508902aba560c91413875e16d6e415
vtagilov <44909454+vtagilov@users.noreply.github.com>
2022-11-06 20:36:30 +0300

    Add files via upload

---
+++ s3_lr2_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+int main()
+{
+    list <char> l;
+
+    char t[2];
+    while (cin)
+    {
+        cin.get(t, 2);
+        l.push_back(*t);
+    }
+
+    for (int i = l.size(); i != 0; i--)
+    {
+        cout << l.back();
+        l.pop_back();
+    }
+}
